67da4a6bee5b853ceacfa95402cc2c6aba7a4ce0
Sanne <sanne.van.mourik@capgemini.com>
2018-10-31 15:52:43 +0100

    feature/NaamVoorFeatureBranch

--- TestBatchFileTwee.bat
+++ TestBatchFileTwee.bat
@@ -9,4 +9,10 @@ git add .
 REM changes lokaal committen
 git commit -m"feature/%1"
 
+REM controleren op conflicten met de origin develop
+git rebase develop
+
+REM feature branch pushen naar origin feature branch
+git push --set-upstream origin feature/%1
+
 
